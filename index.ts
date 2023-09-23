fd600ca8d27522104014d4b784bdad62b75658f5
heko86 <ryou86pg@gmail.com>
2023-09-24 04:08:11 +0900

    インデックスシグネチャ

--- index.ts
+++ index.ts
@@ -1,27 +1,11 @@
-// typeof、keyofのユースケース
-export const obj1 = {
-  foo: "foo",
-  bar: "bar",
+export type User = {
+  name: string;
+  age: number;
+  [key: string]: string | number;
 };
 
-const obj2: typeof obj1 = {
-  foo: "aaa",
-  bar: "bbb",
+const user: User = {
+  name: "miya",
+  account: "miya_it",
+  age: 20,
 };
-
-export function double(x: number | string) {
-  if (typeof x === "string") {
-    return Number(x) * 2;
-  }
-  return x * 2;
-}
-
-export const Obj = {
-  foo: "foo",
-  111: "bar",
-};
-function test(x: keyof typeof Obj) {
-  return;
-}
-
-type Key = keyof typeof Obj;
