bebeefb5de42b55f9c3165b76a42f9a911cfdcd4
heko86 <ryou86pg@gmail.com>
2023-09-24 06:48:00 +0900

    Mapped Types

--- index.ts
+++ index.ts
@@ -1,11 +1,30 @@
 export type User = {
   name: string;
-  age: number;
-  [key: string]: string | number;
+} & OptionalPersonalData;
+
+const foo = {
+  height: 100,
+  weight: 48,
 };
 
+type PersonalData = {
+  height: number;
+  weight: number;
+  realName: string;
+  //   [K in keyof typeof foo]?: number;
+};
+
+type OptionalPersonalData = {
+  [K in keyof PersonalData]?: PersonalData[K];
+  //   height: number;
+  //   weight: number;
+};
+type RequiredPersonalData = {
+  //必須にする　-?
+  [K in keyof PersonalData]-?: PersonalData[K];
+};
 const user: User = {
   name: "miya",
-  account: "miya_it",
-  age: 20,
+  height: 162,
+  weight: 50,
 };
