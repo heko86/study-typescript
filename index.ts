466059a6b77b2e12cf8cdc630df595717e95b705
heko86 <ryou86pg@gmail.com>
2023-09-27 01:08:22 +0900

    Generics

--- index.ts
+++ index.ts
@@ -1,30 +1,55 @@
-export type User = {
+// Generics
+export type User<T> = {
   name: string;
-} & OptionalPersonalData;
-
-const foo = {
-  height: 100,
-  weight: 48,
+  state: T;
 };
 
-type PersonalData = {
-  height: number;
-  weight: number;
-  realName: string;
-  //   [K in keyof typeof foo]?: number;
+type Japanese = User<"東京都" | "大阪府">;
+type American = User<"CA" | "NY">;
+
+const user1: Japanese = {
+  name: "田中",
+  state: "東京都",
+};
+const user2: American = {
+  name: "Johnny",
+  state: "CA",
+};
+// ーーーーーー
+
+// extendsによる型の制約、以下例だとstringと互換性のある型のみしか指定できなくなる
+export type Foo<T extends string | number = string> = {
+  value: T;
 };
 
-type OptionalPersonalData = {
-  [K in keyof PersonalData]?: PersonalData[K];
-  //   height: number;
-  //   weight: number;
+const foo1: Foo = {
+  value: "number",
 };
-type RequiredPersonalData = {
-  //必須にする　-?
-  [K in keyof PersonalData]-?: PersonalData[K];
+const foo2: Foo<number> = {
+  value: 123,
 };
-const user: User = {
-  name: "miya",
-  height: 162,
-  weight: 50,
+// ーーーーーーーー
+// 関数のGenerics
+const foo = <T>(arg: T) => {
+  return { value: arg };
+};
+
+// 引数の型を動的に決めれる
+const foo3 = foo<string>("");
+const foo4 = foo<number>(123);
+
+// 暗黙的な型解決 引数の型を推論してくれるので、毎回型引数を指定しなくてよくなる
+const foo5 = foo("");
+const foo6 = foo(123);
+// Nullableの可能性があるときは時は型引数を指定する
+const a = "";
+const foo7 = foo<string | null>(a);
+
+// ーーーーーーーー
+
+// extendsによる型の制約
+// 重要なポイントとして、extendsがないと、argがunknowになってしまうため、以下のようなtoUpperCaseが呼べなくなる
+const bar = <T extends string>(arg: T) => {
+  arg.toUpperCase();
+  return { value: arg };
 };
