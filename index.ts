fb1808852d7b2a0aa1acd52fcf3121acd423bb57
heko86 <ryou86pg@gmail.com>
2023-10-02 12:09:02 +0900

    infer

--- index.ts
+++ index.ts
@@ -12,3 +12,24 @@ type Filter<T, U> = {
 type StringKeys = Filter<Props, string>;
 type NumberKeys = Filter<Props, number>;
 type booleanKeys = Filter<Props, boolean>;
+
+// infer
+//　例えば以下メソッドの返り値を抽出したいとき、Conditional Typesとinferをあわせることで抽出できる
+const foo1 = () => {
+  return "";
+};
+
+type Return<T> = T extends () => infer U ? U : never;
+
+type Foo2 = Return<typeof foo1>;
+
+// 関数の引数だけを取得したい場合
+const foo2 = (id: string, age: number) => {
+  return "";
+};
+
+type Return2<T> = T extends (...args: [any, infer U, ...any[]]) => any
+  ? U
+  : never;
+
+type Foo3 = Return2<typeof foo2>;
